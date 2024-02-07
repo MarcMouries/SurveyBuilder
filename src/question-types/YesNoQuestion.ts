44cc87f0175dc0e9086d1fdbf9534697eaafcbca
MarcMouries <Marc@Mouries.net>
2024-02-07 10:49:44 -0500

    new

---
+++ src/question-types/YesNoQuestion.ts
@@ -0,0 +1,18 @@
+
+import { OneChoice } from "./OneChoice.ts";
+import type { ISurveyBuilder } from "../ISurveyBuilder.ts";
+import type { IQuestion } from "../IQuestion.ts";
+
+/**
+ * A subclass of OneChoice specifically designed for Yes/No questions. 
+ * It provides a binary choice between two radio buttons labeled 'Yes' and 'No'.
+ * Example Question: "Do you have any previous experience with our products?"
+ * (*) Yes () No
+ */
+export class YesNoQuestion2 extends OneChoice {
+    constructor(surveyBuilder: ISurveyBuilder, question: IQuestion, index: number) {
+        // Temporarily adjust the items for Yes/No questions
+        const modifiedQuestion = { ...question, items: ['Yes', 'No'] };
+        super(surveyBuilder, modifiedQuestion, index);
+    }
+}
