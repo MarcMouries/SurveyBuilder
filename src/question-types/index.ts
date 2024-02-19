4a68b978644319615dbce7843919ca2a6571ab19
MarcMouries <Marc@Mouries.net>
2024-02-19 16:46:26 -0500

    + OneChoice

--- src/question-types/index.ts
+++ src/question-types/index.ts
@@ -2,10 +2,11 @@ export type { IQuestionComponent } from './IQuestionComponent.ts';
 
 export  { FollowUpQuestion } from './FollowUpQuestion.ts';
 export  { MultiLineTextQuestion } from './multi-line.ts';
+export  { OneChoice } from './OneChoice.ts';
 export  { RankingQuestion } from './ranking.ts';
-export  { YesNoQuestion } from './yes-no.ts';
-export  { YesNoQuestion2 } from './YesNoQuestion.ts';
+
 export  { SelectQuestion } from './select.ts';
 export  { SingleLineTextQuestion } from './single-line.ts';
-
+export  { YesNoQuestion } from './yes-no.ts';
+export  { YesNoQuestion2 } from './YesNoQuestion.ts';
 export { SearchInput } from '../component/SearchInput.ts';
