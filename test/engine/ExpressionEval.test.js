8b0bb2d410149109e934298317b70111fc93e057
MarcMouries <marc@mouries.net>
2024-03-16 21:35:35 -0400

    new

--- test/engine/ExpressionEval.test.js
+++ test/engine/ExpressionEval.test.js
@@ -8,8 +8,38 @@ const context = {
     name : "John",
     marital_status : "Married",
     age: 40,
-   }
+   },
 };
+
+const objects= 
+[
+  {
+    "name": "message",
+    "message": "Hello, {{person.name}}!"
+  },
+  {
+    Template: {
+      "name": "message",
+      "message": "Hello, {{person.name}}!"
+    }
+  },
+  {
+    Person: {
+      name : "John",
+    }
+  },
+];
+console.log(objects);
+
+
+test("Evaluate BinaryExpression using nested property 'Person.age' > 18", () => {
+  console.log("\nEvaluating BinaryExpression using nested property 'Person.age'");
+  const complexExpression
+     = new BinaryExpression(new VariableNode("Person.age"), ">", new NumberNode(18));
+  const result = complexExpression.evaluate(context);
+  console.log(`Result: ${result}`);
+  expect(result).toBe(true);
+});
 
 test("Evaluate BooleanNode with true", () => {
   console.log("\nEvaluating BooleanNode with true");
@@ -27,14 +57,7 @@ test("Evaluate BinaryExpression comparing variable 'days' = 10", () => {
   expect(result).toBe(true);
 });
 
-test("Evaluate BinaryExpression using nested property 'Person.age'", () => {
-  console.log("\nEvaluating BinaryExpression using nested property 'Person.age'");
-  const complexExpression
-     = new BinaryExpression(new VariableNode("Person.age"), ">", new NumberNode(18));
-  const result = complexExpression.evaluate(context);
-  console.log(`Result: ${result}`);
-  expect(result).toBe(true);
-});
+
 
   // Construct the expression 2^2
 
