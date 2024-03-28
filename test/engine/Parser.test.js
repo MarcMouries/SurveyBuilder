704e07db12bc2dd352278e7cdd93d80ed1bab5ca
MarcMouries <Marc@Mouries.net>
2024-03-28 11:45:16 -0400

    tests for AssignmentExpression

--- test/engine/Parser.test.js
+++ test/engine/Parser.test.js
@@ -57,24 +57,13 @@ const testCases = [
     expression_string: "age = 18",
     expected: {
       type: "AssignmentExpression",
-      left: {  type: "Identifier", name: "age",
+      left: {
+        type: "Identifier", name: "age",
       },
       right: { type: "Number", value: 18 }
     }
   },
-  {
-    expression_string: "person.age = 18",
-    expected:
-    {
-      "type": "AssignmentExpression",
-      "left": {
-        "type": "MemberExpression",
-        "object": { "type": "Identifier", "name": "person" },
-        "property": { "type": "Identifier", "name": "age" }
-      },
-      "right": { "type": "Number", "value": 18 }
-    }
-  },
+
   {
     expression_string: "age == 18",
     expected: {
@@ -88,20 +77,6 @@ const testCases = [
         type: "Number",
         value: 18
       }
-    }
-  },
-  {
-    expression_string: "person.age == 18",
-    expected:
-    {
-      "type": "BinaryExpression",
-      "operator": "==",
-      "left": {
-        "type": "MemberExpression",
-        "object": { "type": "Identifier", "name": "person" },
-        "property": { "type": "Identifier", "name": "age" }
-      },
-      "right": { "type": "Number", "value": 18 }
     }
   },
   {
@@ -134,9 +109,6 @@ const testCases = [
       }
     }
   },
-
-
-
   {
     expression_string: "10 + 2 * 5",
     expected:
@@ -281,7 +253,37 @@ const testCases = [
       }
     }
   },
-
+  {
+    expression_string: "BMI = weight / height ^ 2",
+    expected:
+    {
+      type: "AssignmentExpression",
+      left: {
+        name: "BMI",
+        type: "Identifier",
+      },
+      right: {
+        "type": "BinaryExpression",
+        "operator": "/",
+        "left": {
+          "type": "Identifier",
+          "name": "weight"
+        },
+        "right": {
+          "type": "BinaryExpression",
+          "operator": "^",
+          "left": {
+            "type": "Identifier",
+            "name": "height"
+          },
+          "right": {
+            "type": "Number",
+            "value": 2
+          }
+        }
+      }
+    }
+  },
   {
     expression_string: "a and b",
     expected: {
@@ -389,39 +391,88 @@ const testCases = [
   //   }
   // },
 
-  
+  {
+    expression_string: "person.age == 18",
+    expected:
+    {
+      "type": "BinaryExpression",
+      "operator": "==",
+      "left": {
+        "type": "MemberExpression",
+        "object": { "type": "Identifier", "name": "person" },
+        "property": { "type": "Identifier", "name": "age" }
+      },
+      "right": { "type": "Number", "value": 18 }
+    }
+  },
+  {
+    expression_string: "person.age = 18",
+    expected:
+    {
+      "type": "AssignmentExpression",
+      "left": {
+        "type": "MemberExpression",
+        "object": { "type": "Identifier", "name": "person" },
+        "property": { "type": "Identifier", "name": "age" }
+      },
+      right: { type: "Number", value: 18 },
+    }
+  },
+  {
+    expression_string: "customer.person.age = 18",
+    expected: {
+      type: "AssignmentExpression",
+      left: {
+        type: "MemberExpression",
+        object: {
+          type: "MemberExpression",
+          object: {
+            name: "customer",
+            type: "Identifier",
+          },
+          property: {
+            name: "person",
+            type: "Identifier",
+          },
+        },
+        property: { name: "age", type: "Identifier" },
+      },
+      right: { type: "Number", value: 18 },
+    }
+  },
   // {
-  //   expression_string: "order.customer.age = 18",
+  //   expression_string: "class Person{}",
   //   expected: {
+  //     type: "AssignmentExpression",
+  //     left: {
+  //       type: "MemberExpression",
+  //       object:   { name: "customer", type: "Identifier" },
+  //       property: { name: "age",      type: "Identifier" },
+  //     },
+  //     right: { type: "Number", value: 18 },
   //   }
   // }
 ];
 
 
 
-/*
-//   "a = 1 + 2",
-//   "eligible = age > 18",
-//   
-//   "BMI = weight / height ^ 2",
-*/
-
 testCases.forEach(({ expression_string, expected }) => {
   test(`Testing '${expression_string}'`, () => {
     const parser = new Parser();
-    console.log(`Input.: '${expression_string}'`);
     const expression = parser.parse(expression_string);
     const expressionJSON = ASTtoJSON.toJson(expression);
-
-    // console.log("expression = ", expression.toJSON())
-    // console.log("summary = " + expression.summarize())
-    const printedExpression = ASTtoString.toString(expression);
-    console.log(`Output: '${printedExpression}'\n`);
-
-    // expect(expression.toJSON()).toEqual(expected);
-    expect(expressionJSON).toEqual(expected);
+    try {
+      expect(expressionJSON).toEqual(expected);
+    } catch (error) {
+      console.log("\n\n");
+      console.log(`Input..: '${expression_string}'`);
+      const printedExpression = ASTtoString.toString(expression);
+      console.log(`Summary: '${printedExpression}'`);
+      console.log(`JSON   : `, expressionJSON);
+      throw error; // Rethrow the error to ensure the test fails.
+    }
   });
-});
+})
 
 //Test for error handling: missing expression after '=' in assignment 
 // const expression_string = "2  3 + 1";
@@ -435,8 +486,8 @@ testCases.forEach(({ expression_string, expected }) => {
 // });
 
 // {
-  //   expression_string: "a = ",
-  //   expected: {
-  //   }
-  // }
-  //   expect(() => tokenizer.parseTokens(test_error)).toThrow("Syntax error: unclosed string literal");
+//   expression_string: "a = ",
+//   expected: {
+//   }
+// }
+//   expect(() => tokenizer.parseTokens(test_error)).toThrow("Syntax error: unclosed string literal");
