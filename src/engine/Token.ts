32ba4a817ced643a3b68f491e2acfd2e54680c79
MarcMouries <Marc@Mouries.net>
2024-03-29 12:35:23 -0400

    new

---
+++ src/engine/Token.ts
@@ -0,0 +1,15 @@
+export const TokenType = {
+    NUMBER: "NUMBER",
+    STRING: "STRING",
+    BOOLEAN: "BOOLEAN",
+    IDENTIFIER: "IDENTIFIER",
+    OPERATOR: "OP",
+    EQUALS: "EQUALS",
+    NOT_EQUAL: "NOT_EQUAL",
+    ASSIGN: "ASSIGN",
+    AND: "AND",
+    OR: "OR",
+    LPAREN: "LPAREN",
+    RPAREN: "RPAREN",
+    DOT: "DOT"
+};
