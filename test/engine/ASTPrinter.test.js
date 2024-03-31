db421e1676ced4bf2d035fe32856a2f841fe5e66
MarcMouries <Marc@Mouries.net>
2024-03-31 09:46:49 -0400

    refactor

--- test/engine/ASTPrinter.test.js
+++ test/engine/ASTPrinter.test.js
@@ -1,40 +1,34 @@
 import { test, expect } from "bun:test";
-import { BooleanNode, BinaryExpression, NumberNode, Identifier } from "../../src/engine/ast/ASTNode";
-import { ASTtoString} from "../../src/engine/ast/ASTtoString";
-import { ASTtoJSON} from "../../src/engine/ast/ASTtoJSON";
-import { Parser} from "../../src/engine/Parser";
+import { ASTtoString } from "../../src/engine/ast/ASTtoString";
+import { ASTtoJSON } from "../../src/engine/ast/ASTtoJSON";
+import { Parser } from "../../src/engine/Parser";
 
-
-test("Evaluate BinaryExpression 'Person.age' > 18", () => {
-    console.log("\nEvaluating BinaryExpression using nested property 'Person.age'");
-    const expression
-       = new BinaryExpression(new Identifier("Person.age"), ">", new NumberNode(18));
-    const expressionToString = ASTtoString.toString(expression);
-    console.log("ASTtoString.toString : ", expressionToString); 
-    const json =  ASTtoJSON.toJson(expression);
-    console.log("ASTtoJSON.toString   : ", json); 
- 
-  });
-
-  test("Test the expression 2*3^2", () => {
-   console.log("\nTest the expression  : '2*3^2'");
-   const parser = new Parser();
-   const expression = parser.parse("2*3^2");
-   const expressionToString = ASTtoString.toString(expression);
-   console.log("ASTtoString.toString : ", expressionToString); 
-   const json =  ASTtoJSON.toJson(expression);
-   console.log("ASTtoJSON.toString   : ", json); 
-
-
-   //expect(result).toBe(18);
- });
- test("Test the expression 2*3^2", () => {
-  console.log("\nTest the expression  : '(2 * (3 ^ 2))'");
+// Helper function to handle repetitive tasks
+function testExpression(input, expected) {
   const parser = new Parser();
-  const expression = parser.parse("(2 * (3 ^ 2))");
-  const expressionToString = ASTtoString.toString(expression);
-  console.log("ASTtoString.toString : ", expressionToString);
-  const json =  ASTtoJSON.toJson(expression);
-  console.log("ASTtoJSON.toString   : ", json); 
-  //expect(result).toBe(18);
-});
+  const expression = parser.parse(input);
+  const result = ASTtoString.toString(expression);
+
+  try {
+    expect(result).toBe(expected);
+  } catch (error) {
+    console.log("Result: : ", result);
+    const json = ASTtoJSON.toJson(expression);
+    console.log("toString: ", json);
+    throw error; // Rethrow the error to ensure the test fails.
+  }
+}
+
+// Function to automatically generate test description and execute test
+function generateTest(input, expected) {
+  test(`Test the expression: ${input}`, () => {
+    testExpression(input, expected);
+  });
+}
+
+// Use the generateTest function to create tests
+generateTest("age > 18",       "(age > 18)");
+
+generateTest("2 * 3 ^ 2",      "(2 * (3 ^ 2))");
+
+generateTest("(2 * (3 ^ 2))",  "(2 * (3 ^ 2))");
