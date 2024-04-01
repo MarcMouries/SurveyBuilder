f071c0a5a7b9c236f0a19f9b21b72c277cc8f012
MarcMouries <Marc@Mouries.net>
2024-04-01 08:43:27 -0400

    + toString()

--- src/engine/Environment.ts
+++ src/engine/Environment.ts
@@ -8,10 +8,23 @@ export class Environment {
     public set(name: string, value: any) {
         this.values.set(name, value);
     }
+    
     public get(name: string): any {
         if (this.values.has(name)) {
             return this.values.get(name);
         }
         throw new Error(`Undefined variable name '${name}'`);
+    }
+
+    public toString(): string {
+        let result = "Environment {";
+        if (this.values.size > 0) {
+            const entries = Array.from(this.values).map(([key, value]) => 
+                `\n    "${key}": ${JSON.stringify(value, null, 4).replace(/\n/g, '\n    ')}`
+            );
+            result += entries.join(',') + '\n';
+        }
+        result += "}";
+        return result;
     }
 }
