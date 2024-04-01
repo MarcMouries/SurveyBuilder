00610b45280eed0c488b57f676c7e3149bce461d
MarcMouries <Marc@Mouries.net>
2024-04-01 15:32:15 -0400

    + tests

--- test/engine/Tokenizer.test.js
+++ test/engine/Tokenizer.test.js
@@ -1,34 +1,60 @@
+import { test, expect } from "bun:test";
 import { TokenType } from "../../src/engine/Token";
 import { Tokenizer } from "../../src/engine/Tokenizer";
 
-//describe("Tokenizer Tests", () => {
-//Test for numeric values
-const test_number = "42";
-test(`Tokenize: '${test_number}'`, () => {
-  console.log(`\nTokenize: '${test_number}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_number);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([{ type: TokenType.NUMBER, value: 42 }]);
-});
+function createTest(input, expected) {
+  test(`Tokenize: ${input}`, () => {
+    const tokenizer = new Tokenizer();
+    const tokens = tokenizer.parseTokens(input);
+    const result = tokens.map(({ type, value }) => ({ type, value }));
+    try {
+      expect(result).toEqual(expected);
+    } catch (error) {
+      console.log("Result: : ", result);
+      throw error; // Rethrow the error to ensure the test fails.
+    }
+  });
+}
 
-const test_float = "37.2";
-test(`Tokenize: '${test_float}'`, () => {
-  console.log(`\nTokenize: '${test_float}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_float);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([{ type: TokenType.NUMBER, value: 37.2 }]);
-});
+// Test for Literals
+createTest("18",   
+[ {    type: "NUMBER",    value: 18  }, ]);
 
-// Test for arithmetic operators
-const test_arithmetic = "1 + 2 - 3 * 4 / 5";
-test(`Tokenize: '${test_arithmetic}'`, () => {
-  console.log(`\nTokenize: '${test_arithmetic}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_arithmetic);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([{ type: TokenType.NUMBER, value: 1 },
+createTest("-18",   
+[ {    type: "OP",        value: "-"  },
+  {    type: "NUMBER",    value: 18  } ]);
+
+createTest("37.2", 
+[ {    type: "NUMBER",    value: 37.2  }, ]);
+
+
+createTest("'A String'", [
+  { type: TokenType.STRING, value: "A String" }]
+);
+
+createTest("true", 
+[  { type: TokenType.BOOLEAN, value: true },]);
+createTest("false", 
+[{ type: TokenType.BOOLEAN, value: false },]);
+
+createTest("!true", 
+[{ type: TokenType.NOT, value: "!" },
+{ type: TokenType.BOOLEAN, value: true }]);
+createTest("!false", 
+[{ type: TokenType.NOT, value: "!" },
+{ type: TokenType.BOOLEAN, value: false }]);
+
+
+createTest("10-5*2", 
+[ {    type: "NUMBER",    value: 10  },
+  {    type: "OP",        value: "-" },
+  {    type: "NUMBER",    value: 5   },
+  {    type: "OP",        value: "*" },
+  {    type: "NUMBER",    value: 2   }
+]);
+
+createTest("1 + 2 - 3 * 4 / 5",
+ [{ type: TokenType.NUMBER, value: 1 },
   { type: TokenType.OPERATOR, value: "+" },
   { type: TokenType.NUMBER, value: 2 },
   { type: TokenType.OPERATOR, value: "-" },
@@ -38,217 +64,27 @@ test(`Tokenize: '${test_arithmetic}'`, () => {
   { type: TokenType.OPERATOR, value: "/" },
   { type: TokenType.NUMBER, value: 5 }
   ]);
-});
 
-// Test for logical operators
-const test_logical = "true and false";
-test(`Tokenize: '${test_logical}'`, () => {
-  console.log(`\nTokenize: : '${test_logical}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_logical);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  console.log("Tokens = ", tokens);
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.BOOLEAN, value: true },
-    { type: TokenType.AND, value: "and" },
-    { type: TokenType.BOOLEAN, value: false },
-  ]);
-});
+createTest("order",
+ [ {    type: "IDENTIFIER",    value: "order"  }, ]);
 
-//Test for error handling - This requires implementing error handling in your tokenizer
-const test_error = "'unclosed string";
-test(`Tokenize: '${test_error}'`, () => {
-  console.log(`\nTokenize: : '${test_error}'`);
-  const tokenizer = new Tokenizer();
-  expect(() => tokenizer.parseTokens(test_error)).toThrow("Syntax error: unclosed string literal");
-});
+ // Test for logical operators
 
-test(`Tokenize: 'ok' (as string)`, () => {
-  const test_string = "'ok'";
-  console.log(`\nTokenize: : '${test_string}'`);
-  const tokens = new Tokenizer().parseTokens(test_string);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([{ type: TokenType.STRING, value: "ok" }]);
-});
+createTest("a or b", 
+[ {    type: "IDENTIFIER",    value: "a"  }, 
+  {    type: "OR",            value: "or" }, 
+  {    type: "IDENTIFIER",    value: "b"  }
+]);
 
-test("Tokenize: 'Object and Field' for 'Person.Name'", () => {
-  const test_object_field = "Person.Name";
-  const tokens = new Tokenizer().parseTokens(test_object_field);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Name" },
-  ]);
-});
-
-test("Tokenize: 'Person.Age is 18'", () => {
-  const test_age_is = "Person.Age is 18";
-  console.log(`\nTokenize: : '${test_age_is}'`);
-  const tokens = new Tokenizer().parseTokens(test_age_is);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Person.Age = 18'", () => {
-  const test_age_equal = "Person.Age = 18";
-  console.log(`\nTokenize: : '${test_age_equal}'`);
-  const tokens = new Tokenizer().parseTokens(test_age_equal);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.ASSIGN, value: "=" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Age != 18'", () => {
-  const test_inequality = "Age != 18";
-  console.log(`\nTokenize: : '${test_inequality}'`);
-  const tokens = new Tokenizer().parseTokens(test_inequality);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.NOT_EQUAL, value: "!=" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Person.Age > 18'", () => {
-  const test_age_greater = "Person.Age > 18";
-  console.log(`\nTokenize: : '${test_age_greater}'`);
-  const tokens = new Tokenizer().parseTokens(test_age_greater);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: ">" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Person.Age <= 18'", () => {
-  const test_age_lesser_equal = "Person.Age <= 18";
-  console.log(`\nTokenize: : '${test_age_lesser_equal}'`);
-  const tokens = new Tokenizer().parseTokens(test_age_lesser_equal);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: "<=" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Person.Age >= 18'", () => {
-  const test_age_greater_equal = "Person.Age >= 18";
-  console.log(`\nTokenize: : '${test_age_greater_equal}'`);
-  const tokens = new Tokenizer().parseTokens(test_age_greater_equal);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: ">=" },
-    { type: TokenType.NUMBER, value: 18 },
-  ]);
-});
-
-test("Tokenize: 'Person.Name is 'John'", () => {
-  const test_IDENTIFIER_is_string = "Person.Name is 'John'";
-  console.log(`\nTokenize: : '${test_IDENTIFIER_is_string}'`);
-  const tokens = new Tokenizer().parseTokens(test_IDENTIFIER_is_string);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Name" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.STRING, value: "John" },
-  ]);
-});
-
-test("Tokenize: 'Person.Name is 'John''", () => {
-  const test_IDENTIFIER_is_string_error = "Person.Name is 'John''";
-  console.log(`\nTokenize: : '${test_IDENTIFIER_is_string_error}'`);
-  const tokenizer = new Tokenizer();
-  expect(() => {
-    tokenizer.parseTokens(test_IDENTIFIER_is_string_error);
-  }).toThrow("Syntax error: unclosed string literal");
-});
+createTest("a and b", 
+[ {    type: "IDENTIFIER",    value: "a"  }, 
+  {    type: "AND",           value: "and" }, 
+  {    type: "IDENTIFIER",    value: "b"  }
+]);
 
 
-test("Tokenize: 'isVisible is true'", () => {
-  const test_is_true = "isVisible is true";
-  const tokens = new Tokenizer().parseTokens(test_is_true);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "isVisible" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.BOOLEAN, value: true },
-  ]);
-});
-
-test("Tokenize: '! true'", () => {
-  const test_is_true = "!true";
-  const tokens = new Tokenizer().parseTokens(test_is_true);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.NOT, value: "!" },
-    { type: TokenType.BOOLEAN, value: true },
-  ]);
-});
-
-test("Tokenize: 'isVisible is true' (Object variable is true)", () => {
-  const object_var_is_true = "isVisible is true";
-  const tokens = new Tokenizer().parseTokens(object_var_is_true);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "isVisible" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.BOOLEAN, value: true },
-  ]);
-});
-
-test("Tokenize: 'is not' phrase", () => {
-  const test_is_not = "isVisible is not true";
-  const tokens = new Tokenizer().parseTokens(test_is_not);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "isVisible" },
-    { type: TokenType.NOT_EQUAL, value: "!=" },
-    { type: TokenType.BOOLEAN, value: true },
-  ]);
-});
-
-test("Tokenize: '2 - -3", () => {
-  const test_minus = "2 - -3";
-  const tokens = new Tokenizer().parseTokens(test_minus);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  try {
-    expect(simplifiedTokens).toEqual([
-      { type: TokenType.NUMBER, value: 2 },
-      { type: TokenType.OPERATOR, value: "-" },
-      { type: TokenType.OPERATOR, value: "-" },
-      { type: TokenType.NUMBER, value: 3 },
-    ]);
-  } catch (error) {
-    console.log("TOKENS = ", tokens);
-    throw error; // Rethrow the error to ensure the test fails.
-  }
-});
-
-test("Tokenize: '1+3'", () => {
+// test to test that the tokenizer returns the proper line and columngit 
+test("Tokenize: 1+3", () => {
   const test_minus = "1+3";
   const tokens = new Tokenizer().parseTokens(test_minus);
   try {
@@ -263,14 +99,126 @@ test("Tokenize: '1+3'", () => {
   }
 });
 
-//Test for combined operators and variables
-const test_combined = "(age + 10) >= 65";
-test(`Tokenize: '${test_combined}'`, () => {
-  console.log(`\nTokenize: : '${test_combined}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_combined);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
+
+
+
+createTest("myList contains 'toto'", 
+[ {     type: "IDENTIFIER",   value: "myList"   },
+  {     type: "CONTAINS",     value: "contains" },
+  {     type: "STRING",       value: "toto"     }
+]); 
+
+createTest("'toto' in myList", 
+[ {     type: "STRING",      value: "toto"   },
+  {     type: "IN",          value: "in" },
+  {     type: "IDENTIFIER",  value: "myList"     }
+]); 
+
+createTest("renewMembership in ['No', 'Undecided']", 
+[ {     type: "IDENTIFIER",  value: "renewMembership"   },
+  {     type: "IN",          value: "in" },
+  {     type: "LBRACKET",    value: "["  },
+  {     type: "STRING",      value: "No" },
+  {     type: "COMMA",       value: "," },
+  {     type: "STRING",      value: "Undecided" },
+  {     type: "RBRACKET",    value: "]"  },
+]); 
+
+// 'Object and Field' for 'Person.Name'
+createTest("Person.Name", [
+  { type: TokenType.IDENTIFIER, value: "Person" },
+  { type: TokenType.DOT, value: "." },
+  { type: TokenType.IDENTIFIER, value: "Name" },
+]);
+
+createTest("Person.Age is 18", [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.EQUALS, value: "==" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);
+
+  createTest("Person.Age == 18",   [
+      { type: TokenType.IDENTIFIER, value: "Person" },
+      { type: TokenType.DOT, value: "." },
+      { type: TokenType.IDENTIFIER, value: "Age" },
+      { type: TokenType.EQUALS, value: "==" },
+      { type: TokenType.NUMBER, value: 18 },
+]); 
+
+createTest("Person.Age = 18", [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.ASSIGN, value: "=" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);
+
+  createTest("Person.Age > 18", [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.OPERATOR, value: ">" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);
+
+  createTest("Age != 18", [
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.NOT_EQUAL, value: "!=" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);
+
+  createTest("Person.Age <= 18", [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.OPERATOR, value: "<=" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);
+
+  createTest("Person.Age  >= 18", [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Age" },
+    { type: TokenType.OPERATOR, value: ">=" },
+    { type: TokenType.NUMBER, value: 18 },
+  ]);  
+
+  createTest("Person.Name is 'John'", 
+  [
+    { type: TokenType.IDENTIFIER, value: "Person" },
+    { type: TokenType.DOT, value: "." },
+    { type: TokenType.IDENTIFIER, value: "Name" },
+    { type: TokenType.EQUALS, value: "==" },
+    { type: TokenType.STRING, value: "John" },
+  ]);
+
+
+
+  createTest("isVisible is true", 
+  [
+    { type: TokenType.IDENTIFIER, value: "isVisible" },
+    { type: TokenType.EQUALS, value: "==" },
+    { type: TokenType.BOOLEAN, value: true },
+  ]);
+
+  createTest("isVisible is not true", 
+  [
+    { type: TokenType.IDENTIFIER, value: "isVisible" },
+    { type: TokenType.NOT_EQUAL, value: "!=" },
+    { type: TokenType.BOOLEAN, value: true },
+  ]);
+
+  createTest("2 - -3", [
+    { type: TokenType.NUMBER, value: 2 },
+    { type: TokenType.OPERATOR, value: "-" },
+    { type: TokenType.OPERATOR, value: "-" },
+    { type: TokenType.NUMBER, value: 3 },
+  ]);
+
+  //Test for combined operators and variables
+  createTest("(age + 10) >= 65", [
     { type: TokenType.LPAREN, value: "(" },
     { type: TokenType.IDENTIFIER, value: "age" },
     { type: TokenType.OPERATOR, value: "+" },
@@ -279,29 +227,22 @@ test(`Tokenize: '${test_combined}'`, () => {
     { type: TokenType.OPERATOR, value: ">=" },
     { type: TokenType.NUMBER, value: 65 },
   ]);
+
+// ERRORS
+
+test("Tokenize: 'Person.Name is 'John''", () => {
+  const test_IDENTIFIER_is_string_error = "Person.Name is 'John''";
+ // console.log(`\nTokenize: : '${test_IDENTIFIER_is_string_error}'`);
+  const tokenizer = new Tokenizer();
+  expect(() => {
+    tokenizer.parseTokens(test_IDENTIFIER_is_string_error);
+  }).toThrow("Syntax error: unclosed string literal");
 });
 
-
-// test that identifier starting with a keyword is tokenized as an identifier
-const test_order = "order";//.age = 18";
-test(`Tokenize: '${test_order}'`, () => {
-  console.log(`\nTokenize: : '${test_order}'`);
+//Test for error handling - This requires implementing error handling in your tokenizer
+const test_error = "'unclosed string";
+test(`Tokenize: '${test_error}'`, () => {
+  console.log(`\nTokenize: : '${test_error}'`);
   const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_order);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "order" },
-  ]);
-});
-const test_or = "a or b";
-test(`Tokenize: '${test_or}'`, () => {
-  console.log(`\nTokenize: : '${test_or}'`);
-  const tokenizer = new Tokenizer();
-  const tokens = tokenizer.parseTokens(test_or);
-  const simplifiedTokens = tokens.map(({ type, value }) => ({ type, value }));
-  expect(simplifiedTokens).toEqual([
-    { type: TokenType.IDENTIFIER, value: "a" },
-    { type: TokenType.OR, value: "or" },
-    { type: TokenType.IDENTIFIER, value: "b" },
-  ]);
+  expect(() => tokenizer.parseTokens(test_error)).toThrow("Syntax error: unclosed string literal");
 });
