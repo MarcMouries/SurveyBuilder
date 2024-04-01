7b5268cf3af2333c28b8d766656d8ab02cdcbb14
MarcMouries <Marc@Mouries.net>
2024-04-01 15:30:35 -0400

    Assignment Expression

--- test/engine/Interpreter.test.js
+++ test/engine/Interpreter.test.js
@@ -1,64 +1,54 @@
-
+import { test, expect } from "bun:test";
 import { Parser } from "../../src/engine/Parser";
 import { Interpreter } from "../../src/engine/Interpreter";
 import { Environment } from "../../src/engine/Environment";
 import { ASTtoJSON } from "../../src/engine/ast/ASTtoJSON";
 
+function createTest(input, expected) {
+  test(`Test the expression: ${input}`, () => {
+    const parser = new Parser();
+    const expression = parser.parse(input);
+    let interpreter = new Interpreter();
+    const result = interpreter.interpret(expression);
+    try {
+      expect(result).toBe(expected);
+    } catch (error) {
+      console.log("Result: : ", result);
+      const json = ASTtoJSON.toJson(expression);
+      console.log("toString: ", json);
+      throw error; // Rethrow the error to ensure the test fails.
+    }
+  });
+}
 
 
-test("Evaluate BinaryExpression using nested property 10-5*2", () => {
-  console.log("\nEvaluating  10-5*2'");
+createTest("18", 18);
+createTest("-18", -18);
+createTest("37.2", 37.2);
+createTest("'A String'", "A String");
+
+createTest("true", true);
+createTest("false", false);
+createTest("!true", false);
+createTest("!false", true);
+
+createTest("20 > 18", true)
+
+createTest("10-5*2", 0);
+createTest("(10-5)*2", 10);
+
+createTest("2^3", 8);
+
+test("Test the expression 'age > 18' with Unknown variable", () => {
   let interpreter = new Interpreter();
   const parser = new Parser();
-  const expression = parser.parse("10-5*2");
-  const result = interpreter.interpret(expression);
-  console.log(`Result: ${result}`);
-  expect(result).toBe(0);
-});
-
-test("Evaluate BinaryExpression using nested property (10-5)*2", () => {
-  console.log("\nEvaluating  (10-5)*2'");
-  let interpreter = new Interpreter();
-  const parser = new Parser();
-  const expression = parser.parse("(10-5)*2");
-  const result = interpreter.interpret(expression);
-  console.log(`Result: ${result}`);
-  expect(result).toBe(10);
-});
-
-test("Evaluate BinaryExpression using nested property 2^2", () => {
-  console.log("\nEvaluating 2^2");
-  let interpreter = new Interpreter();
-  const parser = new Parser();
-  const expression = parser.parse("2^2");
-  const result = interpreter.interpret(expression);
-  console.log(`Result: ${result}`);
-  expect(result).toBe(4);
-});
-
-
-test("Evaluate BinaryExpression 20 > 18", () => {
-  console.log("\nEvaluating 20 > 18");
-  let interpreter = new Interpreter();
-  const parser = new Parser();
-  const expression = parser.parse("20 > 18");
-  const result = interpreter.interpret(expression);
-  console.log(`Result: ${result}`);
-  expect(result).toBe(true);
-});
-
-test("Evaluate Unknown variable in Binary Expression  'age' == 18", () => {
-  console.log("\nEvaluating 'age == 18'");
-  let interpreter = new Interpreter();
-  const parser = new Parser();
-  const expression = parser.parse("age == 18");
+  const expression = parser.parse("age > 18");
   expect(() => {
     interpreter.interpret(expression);
   }).toThrow("Undefined variable name 'age'");
 });
 
-test("Evaluate Known variable in Binary Expression  'height *2 == 10'", () => {
-  console.log("\nEvaluating 'height * 2 == 10'");
+test("Test the expression 'height * 2 == 10' with Known variable", () => {
   let environment = new Environment();
   environment.define("height", 5);
   let interpreter = new Interpreter(environment);
@@ -135,34 +125,9 @@ test("Evaluate nested logical expressions", () => {
   expect(result).toBe(true);
 });
 
-test("Evaluate Unary Negation '-42'", () => {
-  console.log("\nEvaluating '-42'");
-  let environment = new Environment(); // Assuming you have an environment setup
-  let interpreter = new Interpreter(environment);
-  const parser = new Parser();
-  const expression = parser.parse("-42");
-  const result = interpreter.interpret(expression);
-  expect(result).toBe(-42);
-});
 
-test("Evaluate Unary NOT '!true'", () => {
-  console.log("\nEvaluating '!true'");
-  let interpreter = new Interpreter();
-  const parser = new Parser();
-  const expression = parser.parse("! true");
-  console.log(expression);
-  const result = interpreter.interpret(expression);
-  expect(result).toBe(false);
-});
-test("Evaluate Unary NOT '!false'", () => {
-  console.log("\nEvaluating '!false'");
-  let environment = new Environment();
-  let interpreter = new Interpreter(environment);
-  const parser = new Parser();
-  const expression = parser.parse("!false");
-  const result = interpreter.interpret(expression);
-  expect(result).toBe(true);
-});
+
+
 test("Evaluate Unary Negation on variable '-age' where age is 25", () => {
   console.log("\nEvaluating '-age' where age is 25");
   let environment = new Environment();
@@ -173,6 +138,7 @@ test("Evaluate Unary Negation on variable '-age' where age is 25", () => {
   const result = interpreter.interpret(expression);
   expect(result).toBe(-25);
 });
+
 test("Evaluate Unary NOT on variable '!registered' where registered is true", () => {
   console.log("\nEvaluating '!registered' where registered is true");
   let environment = new Environment();
@@ -184,18 +150,61 @@ test("Evaluate Unary NOT on variable '!registered' where registered is true", ()
   expect(result).toBe(false);
 });
 
+test("Evaluate Direct Assignment to a Variable 'x = 10'", () => {
+  console.log("\nEvaluating Direct Assignment 'x = 10'");
+  const parser = new Parser();
+  let expression = parser.parse("x = 10");
+  let environment = new Environment();
+  let interpreter = new Interpreter(environment);
+  //console.log(`Environment: `, environment.toString());
+  let result = interpreter.interpret(expression);
+  console.log(`Result of Assignment: ${result}`);
+  console.log(environment.toString());
 
-//  test("Evaluate Assignment Expression  'person.age' = 18", () => {
-//    console.log("\nEvaluating Assignment 'person.age = 18'");
-//    let interpreter = new Interpreter();
-//    const parser = new Parser();
-//    const expression = parser.parse("person.age = 18");
-//    const json =  ASTtoJSON.toJson(expression);
-//    console.log("ASTtoJSON.toString   : ", json); 
-//    const result = interpreter.interpret(expression);
-//    console.log(`Result: ${result}`);
-//    expect(result).toBe(true);
-//  });
+  // The result of the assignment expression itself should be the value assigned, according to most language specifications
+  expect(result).toBe(10);
+
+  const value = environment.get("x");
+  console.log(`new value in Environment: ${value}`);
+  expect(value).toBe(10);
+
+});
+
+
+
+
+// test("Evaluate Member Expression  'person.age' = 18", () => {
+//   console.log("\nEvaluating Assignment 'person.age = 18'");
+//   const parser = new Parser();
+//   let expression = parser.parse("person.age = 18");
+
+//   let environment = new Environment();
+//   environment.set("person", {});
+//   let interpreter = new Interpreter(environment);
+//   let json = ASTtoJSON.toJson(expression);
+//   console.log("ASTtoJSON.toString   : ", json);
+
+//   let result = interpreter.interpret(expression);
+//   console.log(`Result: ${result}`);
+
+//   expression = parser.parse("person.age == 18");
+//   result = interpreter.interpret(expression);
+//   console.log(`Result: ${result}`);
+//   console.log(`Environment: `, environment.toString());
+//   expect(result).toBe(true);
+// });
+
+// test("Evaluate Assignment Expression  'person.age' = 18", () => {
+//   console.log("\nEvaluating Assignment 'person.age = 18'");
+//   let interpreter = new Interpreter();
+//   const parser = new Parser();
+//   const expression = parser.parse("person.age = 18");
+//   const json = ASTtoJSON.toJson(expression);
+//   console.log("ASTtoJSON.toString   : ", json);
+//   const result = interpreter.interpret(expression);
+//   console.log(`Result: ${result}`);
+//   expect(result).toBe(true);
+// });
 
 //  test("Evaluate Assignment Expression  'order.customer.age' = 18", () => {
 //   console.log("\nEvaluating Assignment 'order.customer.age = 18'");
