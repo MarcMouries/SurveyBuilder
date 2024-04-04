5f31874bf2f7e3548c27e7e35d7ce7046da7be78
MarcMouries <Marc@Mouries.net>
2024-04-04 15:16:43 -0400

    additional tests

--- test/engine/Tokenizer.test.js
+++ test/engine/Tokenizer.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "bun:test";
-import { TokenType } from "../../src/engine/Token";
+import { Token } from "../../src/engine/Token";
 import { Tokenizer } from "../../src/engine/Tokenizer";
 
 function createTest(input, expected) {
@@ -29,20 +29,20 @@ createTest("37.2",
 
 
 createTest("'A String'", [
-  { type: TokenType.STRING, value: "A String" }]
+  { type: Token.STRING, value: "A String" }]
 );
 
 createTest("true", 
-[  { type: TokenType.BOOLEAN, value: true },]);
+[  { type: Token.BOOLEAN, value: true },]);
 createTest("false", 
-[{ type: TokenType.BOOLEAN, value: false },]);
+[{ type: Token.BOOLEAN, value: false },]);
 
 createTest("!true", 
-[{ type: TokenType.NOT, value: "!" },
-{ type: TokenType.BOOLEAN, value: true }]);
+[{ type: Token.NOT, value: "!" },
+{ type: Token.BOOLEAN, value: true }]);
 createTest("!false", 
-[{ type: TokenType.NOT, value: "!" },
-{ type: TokenType.BOOLEAN, value: false }]);
+[{ type: Token.NOT, value: "!" },
+{ type: Token.BOOLEAN, value: false }]);
 
 
 createTest("10-5*2", 
@@ -53,16 +53,27 @@ createTest("10-5*2",
   {    type: "NUMBER",    value: 2   }
 ]);
 
+createTest("(10-5)*2",
+[{    type: "LPAREN",    value: "(" },
+ {    type: "NUMBER",    value: 10  },
+ {    type: "OP",        value: "-" },
+ {    type: "NUMBER",    value: 5   },
+ {    type: "RPAREN",    value: ")" },
+ {    type: "OP",        value: "*" },
+ {    type: "NUMBER",    value: 2   }
+]);
+
+
 createTest("1 + 2 - 3 * 4 / 5",
- [{ type: TokenType.NUMBER, value: 1 },
-  { type: TokenType.OPERATOR, value: "+" },
-  { type: TokenType.NUMBER, value: 2 },
-  { type: TokenType.OPERATOR, value: "-" },
-  { type: TokenType.NUMBER, value: 3 },
-  { type: TokenType.OPERATOR, value: "*" },
-  { type: TokenType.NUMBER, value: 4 },
-  { type: TokenType.OPERATOR, value: "/" },
-  { type: TokenType.NUMBER, value: 5 }
+ [{ type: Token.NUMBER, value: 1 },
+  { type: Token.OPERATOR, value: "+" },
+  { type: Token.NUMBER, value: 2 },
+  { type: Token.OPERATOR, value: "-" },
+  { type: Token.NUMBER, value: 3 },
+  { type: Token.OPERATOR, value: "*" },
+  { type: Token.NUMBER, value: 4 },
+  { type: Token.OPERATOR, value: "/" },
+  { type: Token.NUMBER, value: 5 }
   ]);
 
 createTest("order",
@@ -89,9 +100,9 @@ test("Tokenize: 1+3", () => {
   const tokens = new Tokenizer().parseTokens(test_minus);
   try {
     expect(tokens).toEqual([
-      { type: TokenType.NUMBER, value: 1, line: 1, column: 1 },
-      { type: TokenType.OPERATOR, value: "+", line: 1, column: 2 },
-      { type: TokenType.NUMBER, value: 3, line: 1, column: 3 },
+      { type: Token.NUMBER, value: 1, line: 1, column: 1 },
+      { type: Token.OPERATOR, value: "+", line: 1, column: 2 },
+      { type: Token.NUMBER, value: 3, line: 1, column: 3 },
     ]);
   } catch (error) {
     console.log("TOKENS = ", tokens);
@@ -108,14 +119,14 @@ createTest("myList contains 'toto'",
   {     type: "STRING",       value: "toto"     }
 ]); 
 
-createTest("'toto' in myList", 
-[ {     type: "STRING",      value: "toto"   },
+createTest("'blue' in List_of_Colors", 
+[ {     type: "STRING",      value: "blue"   },
   {     type: "IN",          value: "in" },
-  {     type: "IDENTIFIER",  value: "myList"     }
+  {     type: "IDENTIFIER",  value: "List_of_Colors"     }
 ]); 
 
-createTest("renewMembership in ['No', 'Undecided']", 
-[ {     type: "IDENTIFIER",  value: "renewMembership"   },
+createTest("answer in ['No', 'Undecided']", 
+[ {     type: "IDENTIFIER",  value: "answer"},
   {     type: "IN",          value: "in" },
   {     type: "LBRACKET",    value: "["  },
   {     type: "STRING",      value: "No" },
@@ -126,106 +137,106 @@ createTest("renewMembership in ['No', 'Undecided']",
 
 // 'Object and Field' for 'Person.Name'
 createTest("Person.Name", [
-  { type: TokenType.IDENTIFIER, value: "Person" },
-  { type: TokenType.DOT, value: "." },
-  { type: TokenType.IDENTIFIER, value: "Name" },
+  { type: Token.IDENTIFIER, value: "Person" },
+  { type: Token.DOT, value: "." },
+  { type: Token.IDENTIFIER, value: "Name" },
 ]);
 
 createTest("Person.Age is 18", [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.EQUALS, value: "==" },
+    { type: Token.NUMBER, value: 18 },
   ]);
 
   createTest("Person.Age == 18",   [
-      { type: TokenType.IDENTIFIER, value: "Person" },
-      { type: TokenType.DOT, value: "." },
-      { type: TokenType.IDENTIFIER, value: "Age" },
-      { type: TokenType.EQUALS, value: "==" },
-      { type: TokenType.NUMBER, value: 18 },
+      { type: Token.IDENTIFIER, value: "Person" },
+      { type: Token.DOT, value: "." },
+      { type: Token.IDENTIFIER, value: "Age" },
+      { type: Token.EQUALS, value: "==" },
+      { type: Token.NUMBER, value: 18 },
 ]); 
 
 createTest("Person.Age = 18", [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.ASSIGN, value: "=" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.ASSIGN, value: "=" },
+    { type: Token.NUMBER, value: 18 },
   ]);
 
   createTest("Person.Age > 18", [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: ">" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.OPERATOR, value: ">" },
+    { type: Token.NUMBER, value: 18 },
   ]);
 
   createTest("Age != 18", [
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.NOT_EQUAL, value: "!=" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.NOT_EQUAL, value: "!=" },
+    { type: Token.NUMBER, value: 18 },
   ]);
 
   createTest("Person.Age <= 18", [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: "<=" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.OPERATOR, value: "<=" },
+    { type: Token.NUMBER, value: 18 },
   ]);
 
   createTest("Person.Age  >= 18", [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Age" },
-    { type: TokenType.OPERATOR, value: ">=" },
-    { type: TokenType.NUMBER, value: 18 },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Age" },
+    { type: Token.OPERATOR, value: ">=" },
+    { type: Token.NUMBER, value: 18 },
   ]);  
 
   createTest("Person.Name is 'John'", 
   [
-    { type: TokenType.IDENTIFIER, value: "Person" },
-    { type: TokenType.DOT, value: "." },
-    { type: TokenType.IDENTIFIER, value: "Name" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.STRING, value: "John" },
+    { type: Token.IDENTIFIER, value: "Person" },
+    { type: Token.DOT, value: "." },
+    { type: Token.IDENTIFIER, value: "Name" },
+    { type: Token.EQUALS, value: "==" },
+    { type: Token.STRING, value: "John" },
   ]);
 
 
 
   createTest("isVisible is true", 
   [
-    { type: TokenType.IDENTIFIER, value: "isVisible" },
-    { type: TokenType.EQUALS, value: "==" },
-    { type: TokenType.BOOLEAN, value: true },
+    { type: Token.IDENTIFIER, value: "isVisible" },
+    { type: Token.EQUALS, value: "==" },
+    { type: Token.BOOLEAN, value: true },
   ]);
 
   createTest("isVisible is not true", 
   [
-    { type: TokenType.IDENTIFIER, value: "isVisible" },
-    { type: TokenType.NOT_EQUAL, value: "!=" },
-    { type: TokenType.BOOLEAN, value: true },
+    { type: Token.IDENTIFIER, value: "isVisible" },
+    { type: Token.NOT_EQUAL, value: "!=" },
+    { type: Token.BOOLEAN, value: true },
   ]);
 
   createTest("2 - -3", [
-    { type: TokenType.NUMBER, value: 2 },
-    { type: TokenType.OPERATOR, value: "-" },
-    { type: TokenType.OPERATOR, value: "-" },
-    { type: TokenType.NUMBER, value: 3 },
+    { type: Token.NUMBER, value: 2 },
+    { type: Token.OPERATOR, value: "-" },
+    { type: Token.OPERATOR, value: "-" },
+    { type: Token.NUMBER, value: 3 },
   ]);
 
   //Test for combined operators and variables
   createTest("(age + 10) >= 65", [
-    { type: TokenType.LPAREN, value: "(" },
-    { type: TokenType.IDENTIFIER, value: "age" },
-    { type: TokenType.OPERATOR, value: "+" },
-    { type: TokenType.NUMBER, value: 10 },
-    { type: TokenType.RPAREN, value: ")" },
-    { type: TokenType.OPERATOR, value: ">=" },
-    { type: TokenType.NUMBER, value: 65 },
+    { type: Token.LPAREN, value: "(" },
+    { type: Token.IDENTIFIER, value: "age" },
+    { type: Token.OPERATOR, value: "+" },
+    { type: Token.NUMBER, value: 10 },
+    { type: Token.RPAREN, value: ")" },
+    { type: Token.OPERATOR, value: ">=" },
+    { type: Token.NUMBER, value: 65 },
   ]);
 
 // ERRORS
@@ -242,7 +253,7 @@ test("Tokenize: 'Person.Name is 'John''", () => {
 //Test for error handling - This requires implementing error handling in your tokenizer
 const test_error = "'unclosed string";
 test(`Tokenize: '${test_error}'`, () => {
-  console.log(`\nTokenize: : '${test_error}'`);
+  //console.log(`\nTokenize: : '${test_error}'`);
   const tokenizer = new Tokenizer();
   expect(() => tokenizer.parseTokens(test_error)).toThrow("Syntax error: unclosed string literal");
 });
