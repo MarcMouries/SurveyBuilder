02ca9db15cacc1cd255a2fad4d02a31f6330a89a
MarcMouries <Marc@Mouries.net>
2024-04-12 07:51:38 -0400

    set some attributes optional

--- src/IQuestion.ts
+++ src/IQuestion.ts
@@ -2,13 +2,14 @@ export interface IQuestion {
 
     title: string;
     name: string;
-    isRequired: boolean;
+    isRequired?: boolean;
+    isVisible? : boolean;
     type: string;
-    items : string[];
-    options : string[];
-    options_source: string[];
-    visible_when: string;
-    includeOtherOption: any;
-    placeholder : string;
+    items? : string[];
+    options? : string[];
+    options_source?: string[];
+    visible_when?: string;
+    includeOtherOption?: any;
+    placeholder? : string;
     detailQuestions?: IQuestion[];
 }
