c1798240e82322cb1b168857484bc2912d68ade9
MarcMouries <Marc@Mouries.net>
2024-04-23 07:45:28 -0400

    (-) evaluateVisibilityConditions

--- src/ISurveyBuilder.ts
+++ src/ISurveyBuilder.ts
@@ -4,5 +4,4 @@ import type { IQuestionResponse } from './question-types/IQuestionResponse.ts';
 export interface ISurveyBuilder {
     addQuestionElement(questionDiv: HTMLDivElement): void;
     setResponse (response: IQuestionResponse): void;
-    evaluateVisibilityConditions(response: IQuestionResponse): void;
 }
