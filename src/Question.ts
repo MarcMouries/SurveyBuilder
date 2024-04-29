f09a7bcc240981205c6e86157b4e35a82fa4dfb0
MarcMouries <Marc@Mouries.net>
2024-04-29 15:52:59 -0400

    new

---
+++ src/Question.ts
@@ -0,0 +1,35 @@
+import type { IQuestion } from './IQuestion.ts';
+
+export class Question implements IQuestion {
+    static currentQuestionNumber: number = 0;
+
+    index: number;
+    title: string;
+    name: string;
+    isRequired?: boolean | undefined;
+    isVisible?: boolean | undefined;
+    type: string;
+    items?: string[] | undefined;
+    options?: string[] | undefined;
+    options_source?: string[] | undefined;
+    visible_when?: string | undefined;
+    includeOtherOption?: any;
+    placeholder?: string | undefined;
+    detailQuestions?: IQuestion[] | undefined;
+
+    constructor(data: Omit<IQuestion, 'index'>) {
+        this.index = Question.currentQuestionNumber++;
+        this.title = data.title;
+        this.name = data.name;
+        this.type = data.type;
+        this.isRequired = data.isRequired;
+        this.isVisible = typeof data.isVisible === 'boolean' ? data.isVisible : true;
+        this.items = data.items;
+        this.options = data.options;
+        this.options_source = data.options_source;
+        this.visible_when = data.visible_when;
+        this.includeOtherOption = data.includeOtherOption;
+        this.placeholder = data.placeholder;
+        this.detailQuestions = data.detailQuestions;
+      }
+    }
