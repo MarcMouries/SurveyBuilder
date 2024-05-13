222fea1aa8c482182851a60e557999337ec315df
MarcMouries <Marc@Mouries.net>
2024-05-13 13:30:31 -0400

    refactoring

--- src/question-types/IQuestionComponent.ts
+++
@@ -1,9 +0,0 @@
-import type { IQuestion } from "../IQuestion";
-
-export interface IQuestionComponent {
-    questionData: IQuestion;
-    getQuestionDiv(): HTMLDivElement;
-    show() : void;
-    hide() : void;
-    setTitle(title: string): void;
-}
